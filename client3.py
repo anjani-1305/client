10c907854b059fac7fa05092338bf550035df29e
Anjani <kanjani142@gmail.com>
2023-09-09 13:16:36 +0530

    madegetDataPOint

--- client3.py
+++ client3.py
@@ -3,4 +3,11 @@ class StockDataProcessor:
         self.stock_A_data = stock_A_data
         self.stock_B_data = stock_B_data
 
+    def getDataPoint(self, stock_data):
+        stock_name = stock_data['name']
+        bid_price = stock_data['bid_price']
+        ask_price = stock_data['ask_price']
+        price = (bid_price + ask_price) / 2
+        return (stock_name, bid_price, ask_price, price)
+
 
